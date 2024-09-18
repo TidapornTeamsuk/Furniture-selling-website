14e1248ad7433b64406a5a4307155b007d4dcdff
punho1 <tidapornterm2018@gmail.com>
2024-09-18 18:06:52 +0700

    add controller

---
+++ demo/src/main/java/com/housely/demo/controller/AdminController.java
@@ -0,0 +1,9 @@
+package com.housely.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdminController {
+    
+    
+}
--- demo/src/main/java/com/housely/demo/controller/HouselyController.java
+++ demo/src/main/java/com/housely/demo/controller/HouselyController.java
@@ -1,4 +1,4 @@
-package com.housely.demo;
+package com.housely.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
